e6d21be20844bfefda3f4dbaa8a95319b3a4707f
Mauricio Ocando <mauroocando@gmail.com>
2022-04-23 03:07:27 -0400

    add delete logic

--- api/src/posts/services/posts.service.ts
+++ api/src/posts/services/posts.service.ts
@@ -1,30 +1,18 @@
-import { HttpService } from '@nestjs/axios';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { Inject, Injectable } from '@nestjs/common';
-import { firstValueFrom } from 'rxjs';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Post } from '../entities/posts.entity';
-import { Db } from 'mongodb';
 
 @Injectable()
 export class PostsService {
-  constructor(
-    @Inject('MONGO') private database: Db,
-    @InjectModel(Post.name) private postModel: Model<Post>,
-  ) {}
-  //logica de rutas
+  constructor(@InjectModel(Post.name) private postModel: Model<Post>) {}
 
-  async findAll() {
+  findAll() {
     return this.postModel.find().exec();
-
-    // const response = this.httpService.get(
-    //   'https://hn.algolia.com/api/v1/search_by_date?query=nodejs',
-    // );
-    // const posts = await firstValueFrom(response);
-    // return posts.data;
   }
 
-  deleteOne(id: string) {
-    return `delete ${id} post`;
+  async deleteOne(postId: string) {
+    const result = await this.postModel.findByIdAndDelete(postId);
+    if (!result) throw new NotFoundException();
   }
 }
